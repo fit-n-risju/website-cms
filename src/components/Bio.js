87ebc79dfa9a4a9cc95ecd377317f9b888f0c267
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-04-30 22:47:11 -0400

    Optimized about page for medium screen sizes

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -10,8 +10,9 @@ const Bio = () => (
     <div>
     <h2>HI, I'M JULIA!</h2>
     <br /><br />
-    <p>How can you stay fit, build muscles, eat healthy, supply your body with all the necessary vitamins and nutrients, and all this with minimal effort?
-      <br /><br />We want to answer this question together here, through mutual exchange with you.</p>
+    <p>Over 10 years ago I was diagnosed with celiac disease (gluten intolerance). Because of this diagnosis, I have a lot of experience with the gluten-free diet and know how it works and what to look out for.<br /><br />
+    I have also developed strategies on how to harmonize these diets with a vegan, balanced and healthy diet. In addition, weight lifting has been an important part of my life since 2013, which also led to a further change in my diet. <br /><br />
+    Over the years I have worked on harmoniously bringing these components together and developing them further. I‘m excited to share this knowledge and experience with you and support you on your way to a healthy and balanced life!</p>
     </div>
   </div>
 </div>
@@ -21,8 +22,9 @@ const Bio = () => (
     <div>
     <h2>HEY THERE, I'M CHRIS!</h2>
     <br /><br />
-    <p>How can you stay fit, build muscles, eat healthy, supply your body with all the necessary vitamins and nutrients, and all this with minimal effort?
-      <br /><br />We want to answer this question together here, through mutual exchange with you.</p>
+    <p>Due to my ambitious study program, I quickly came across the challenge of combining my passion for weight lifting and the goal of eating and living healthy. I have made it my task to optimize the sport and the conscious, healthy eating for me despite lack of time.<br /><br /> 
+    I am always accompanied by the question "How do I provide my body with everything it needs?" I've always been open to trying new things so I've been through almost every diet. I am also a fighter who tries to push himself to the limit in every training session.<br /><br />
+    In 2015 I decided to go vegan for several reasons. Today I found the perfect balance for my body and I feel comfortable with it. I don't want to pass this knowledge only to friends, I want to share it with everyone who wants to give their body what it deserves with little effort.</p>
     </div>
   </div>
   <div className="bio-pic chris">
